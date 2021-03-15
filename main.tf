6292530ddc65af90d301ad34914a6f43330310de
p-ebot <peterebot@yahoo.com>
2021-03-15 16:35:35 -0400

    ec2 instance and tag

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_instance" "example" {
+  ami           = "ami-038f1ca1bd58a5790"
+  instance_type = "t2.micro"
+
+tags = {
+  Name = "terraform_test_server"
+  }
+}
