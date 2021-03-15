2cdf841e71122ddf9e36e9164acba0ee013b5f69
p-ebot <peterebot@yahoo.com>
2021-03-15 16:54:34 -0400

    EC2 SG

--- main.tf
+++ main.tf
@@ -5,8 +5,25 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-038f1ca1bd58a5790"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
 tags = {
   Name = "terraform_test_server"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-test-SG"
+  
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
